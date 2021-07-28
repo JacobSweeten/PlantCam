e7597721c712b4fc80557b8b81804617edcc7fed
JacobSweeten <redacted>
2021-07-27 20:39:51 -0500

    Added download functionality via webserver

--- Node/index.js
+++ Node/index.js
@@ -1,6 +1,8 @@
 const NodeWebcam = require("node-webcam");
 const ini = require("ini");
+const http = require("http");
 const fs = require("fs");
+const AdmZip = require("adm-zip");
 
 var opts = {
 	width: 1280,
@@ -15,6 +17,9 @@ var opts = {
 	verbose: false
 };
 
+var indexFile = fs.readFileSync("./displayImages.html");
+var styleFile = fs.readFileSync("./displayImages.css");
+
 try
 {
 	var configFile = fs.readFileSync("./config.ini", "utf-8");
@@ -22,7 +27,7 @@ try
 catch(e)
 {
 	console.error("config.ini does not exist. Creating.");
-	fs.writeFileSync("[PlantCam]\nInterval = 86400\nSetTime = 12:00\nDir = ./images\nMode = SetTime");
+	fs.writeFileSync("[PlantCam]\nInterval = 86400\nSetTime = 12:00\nMode = SetTime");
 	process.exit(1);
 }
 
@@ -36,9 +41,9 @@ catch(e)
 	process.exit(1);
 }
 
-if(!fs.existsSync(config.PlantCam.Dir))
+if(!fs.existsSync("./images"))
 {
-	fs.mkdirSync(config.PlantCam.Dir);
+	fs.mkdirSync("./images");
 }
 
 var webcam = NodeWebcam.create(opts);
@@ -48,7 +53,7 @@ function takePicture()
 	var dateTime = new Date().toISOString();
 	var dateTime = dateTime.replace(/T/, '_').replace(/\..+/, '');
 	
-	webcam.capture(config.PlantCam.Dir + "/" +  dateTime + ".jpeg", (err, data) => {
+	webcam.capture("./images/" +  dateTime + ".jpeg", (err, data) => {
 		if(err)
 		{
 			console.log(err);
@@ -81,3 +86,74 @@ else if(config.PlantCam.Mode == "SetTime")
 		}
 	}, 1000);
 }
+
+const httpServer = http.createServer((req, res) => {
+	if(/^\/(index\.(htm|html))?$/.test(req.url))
+	{
+		res.writeHead(200);
+
+		var insertString = "";
+		var imgList = fs.readdirSync("./images");
+
+		for(var i = 0; i < imgList.length; i++)
+		{
+			insertString = insertString + "<img src=\"/images/" + imgList[i] + "\">\n<br>\n";
+		}
+
+		var tempFile = indexFile.toString().replace(/<!--INSERT-->/, insertString);
+
+		res.end(tempFile);
+	}
+	else if(req.url === "/displayImages.css")
+	{
+		res.writeHead(200);
+		res.end(styleFile);
+	}
+	else if(req.url.startsWith("/images/"))
+	{
+		var imgString = req.url.split("/")[2];
+
+		if(/(\.\.)/.test(req.url))
+		{
+			res.writeHead(404);
+			res.end("Not found");
+		}
+		else if(fs.existsSync("./images/" + imgString))
+		{
+			res.writeHead(200);
+			res.end(fs.readFileSync("./images/" + imgString));
+		}
+		else
+		{
+			res.writeHead(404);
+			res.end("Not found");
+		}
+	}
+	else if(req.url.split("?")[0] === "/download.zip")
+	{
+		res.writeHead(200);
+
+		var zip = new AdmZip();
+
+		var imgList = fs.readdirSync("./images");
+
+		for(var i = 0; i < imgList.length; i++)
+		{
+			zip.addLocalFile("./images/" + imgList[i]);
+		}
+
+		zip.writeZip("./download.zip");
+
+		zipFile = fs.readFileSync("./download.zip");
+		res.end(zipFile);
+
+		fs.rmSync("./download.zip");
+	}
+	else
+	{
+		res.writeHead(404);
+		res.end("Not found");
+	}
+});
+
+httpServer.listen(8080);
